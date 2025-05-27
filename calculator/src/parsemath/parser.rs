65f42052756f6912771ef55704ca97ec8f76b136
Sohan Nannra <49798755+snannra@users.noreply.github.com>
2025-05-26 23:38:01 -0700

    completed the parser

--- calculator/src/parsemath/parser.rs
+++ calculator/src/parsemath/parser.rs
@@ -27,10 +27,60 @@ impl<'a> Parser<'a> {
     fn get_next_token(&mut self) -> Result<(), ParseError> {
         let next_token = match self.tokenizer.next() {
             Some(token) => token,
-            None => return Err(ParseError::InvalidOperator
-                ("Invalid character".into())),
+            None => return Err(ParseError::InvalidOperator("Invalid character".into())),
         };
         self.current_token = next_token;
         Ok(())
+    }
+
+    fn check_paren(&mut self, expected: Token) -> Result<(), ParseError> {
+        if expected == self.current_token {
+            self.get_next_token()?;
+            OK(())
+        } else {
+            Err(ParseError::InvalidOperator(format!(
+                "Expected {:?}, got {:?}",
+                expected, self.current_token
+            )))
+        }
+    }
+
+    fn parse_number(&mut self) -> Result<Node, ParseError> {
+        let token = self.current_token.clone();
+        match token {
+            Token::Subtract => {
+                self.get_next_token()?;
+                let expr = self.generate_ast(OperPrec::Negative)?;
+                Ok(Node::Negative(Box::new(expr)))
+            }
+            Token::Num(i) => {
+                self.get_next_token()?;
+                Ok(Node::Number(i))
+            }
+            Token::LeftParen => {
+                self.get_next_token()?;
+                let expr = self.generate_ast(OperPrec::DefaultZero)?;
+                self.check_paren(Token::RightParen)?;
+                if self.current_token == Token::LeftParen {
+                    let right = self.generate_ast(OperPrec::MulDiv)?;
+                    return Ok(Node::Multiply(Box::new(expr), Box::new(right)));
+                }
+                Ok(expr)
+            }
+            _ => Err(ParseError::UnableToParse("Unable to parse".to_string())),
+        }
+    }
+
+    fn generate_ast(&mut self, oper_prec: OperPrec) -> Result<Node, ParseError> {
+        let mut left_expr = self.parse_number()?;
+
+        while oper_prec < self.current_token.get_oper_prec() {
+            if self.current_token == Token::EOF {
+                break;
+            }
+            let right_expr = self.convert_token_to_node(left_expr.clone())?;
+            left_expr = right_expr;
+        }
+        Ok(left_expr)
     }
 }
